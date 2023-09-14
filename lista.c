1a9866740ca529b5caf61167dd5edd84d75bdd50
arthur oliveira <129237472+arthurzi03@users.noreply.github.com>
2023-09-14 09:43:20 -0300

    Add files via upload

---
+++ lista.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    int a,b,c,maior;
+    scanf("%d %d %d",&a,&b,&c);
+    maior=(a + b + abc(a-b))/2;
+    printf("%d eh o maior\n",maior);
+     return 0;
+}
